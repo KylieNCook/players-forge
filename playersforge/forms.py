c27bb4afbd84d22f905d2b58d18a7ea71a2e6507
KylieNCook <kyliencook@gmail.com>
2021-03-26 13:51:59 -0700

    created a login and profile page, added user tracking by login and logout

--- playersforge/forms.py
+++ playersforge/forms.py
@@ -4,7 +4,14 @@ from wtforms.validators import DataRequired
 
 # form used in signup.html to gather information given by user
 # also used in app.py/signup to transfer data into app.py variables
-class RegisterForm(FlaskForm):
+class SignUpForm(FlaskForm):
     email = StringField("Email", validators=[DataRequired()])
     username = StringField("Username", validators=[DataRequired()])
     password = PasswordField("Password", validators=[DataRequired()])
+
+# form used in signup.html to gather information given by user
+# also used in app.py/signup to transfer data into app.py variables
+class LoginForm(FlaskForm):
+    email = StringField("Email", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired()])
+    
--- playersforge/models.py
+++ playersforge/models.py
@@ -1,12 +1,17 @@
 from flask_sqlalchemy import SQLAlchemy
 from config import app
+from flask_login import LoginManager, UserMixin
 
 # connect to the database
 db = SQLAlchemy(app)
 
+# starts login manager class
+login_manager = LoginManager()
+login_manager.init_app(app)
+
 # Users database model that carries an id (automatically increments in database),
 # email, username, and password
-class Users(db.Model):
+class Users(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(50), nullable=False)
     username = db.Column(db.String(25), unique=True, nullable=False)
@@ -16,3 +21,10 @@ class Users(db.Model):
         self.email = email
         self.username = username
         self.password = password
+
+    # returns everything associated to the input user id
+    @login_manager.user_loader
+    def load_user(user_id):
+        return Users.query.get(int(user_id))
+
+    
